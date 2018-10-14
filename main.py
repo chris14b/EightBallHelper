a0341213b77dbd1ec53c186654206460fb5a630f
Chris Johnston <chris14b@gmail.com>
2018-10-14 14:42:01 +1100

    Added a Maths class for a few mathematical functions, and also a Point class to make positions easier to handle.

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ class Table:
         output = self.image
 
         for pocket in self.pockets:
-            cv2.circle(output, pocket.position, pocket.radius, Colour.POCKET, 2)  # show a red circle for each pocket
+            # show a red circle for each pocket
+            cv2.circle(output, pocket.position.tuple(), pocket.radius, Colour.POCKET, 2)
 
         for ball in self.balls:
             if ball.type == "solids":
@@ -25,16 +26,19 @@ class Table:
             else:
                 colour = Colour.CUE  # show a white circle for any other ball type
 
-            cv2.circle(output, ball.position, ball.radius, colour, -1)
+            cv2.circle(output, ball.position.tuple(), ball.radius, colour, -1)
 
         shot = self.__calculate_best_shot(ball_type)  # determine the best shot
 
-        phantom_cue_ball = shot.create_phantom_cue_ball()  # location of cue ball when it hits target ball
-        cv2.circle(output, phantom_cue_ball.position, phantom_cue_ball.radius, Colour.CUE, 1)  # show phantom cue ball
+        if shot:
+            phantom_cue_ball = shot.create_phantom_cue_ball()  # location of cue ball when it hits target ball
+            cv2.circle(output, phantom_cue_ball.position.tuple(), phantom_cue_ball.radius, Colour.CUE, 1)
 
-        # show ball trajectories
-        cv2.line(output, shot.cue_ball.position, phantom_cue_ball.position, Colour.CUE)
-        cv2.line(output, shot.target_ball.position, shot.pocket.position, Colour.CUE)
+            # show ball trajectories
+            cv2.line(output, shot.cue_ball.position.tuple(), phantom_cue_ball.position.tuple(), Colour.CUE)
+            cv2.line(output, shot.target_ball.position.tuple(), shot.pocket.position.tuple(), Colour.CUE)
+        else:
+            cv2.putText(output, "No good shots available", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, Colour.RED)
 
         cv2.imshow("Output", output)
         cv2.waitKey(0)
@@ -48,7 +52,10 @@ class Table:
     # return the shot with the least angle for a ball of type ball_type
     def __calculate_best_shot(self, ball_type):
         possible_shots = self.__calculate_possible_shots(ball_type)
-        # TODO: what if there are no possible shots?
+
+        if len(possible_shots) == 0:
+            return None
+
         return min(possible_shots, key=lambda p: p.angle)
 
     # calculate all shots with an angle of less than 90 degree for a ball of type ball_type
@@ -57,12 +64,12 @@ class Table:
         possible_shots = []  # initial array of possible shots
 
         # loop over balls and pockets and create a shot object for every one
-        for ball in self.balls:
-            if ball.type != ball_type:
+        for target_ball in self.balls:
+            if target_ball.type != ball_type:
                 continue
 
             for pocket in self.pockets:
-                shot = Shot(cue_ball, ball, pocket)
+                shot = Shot(cue_ball, target_ball, pocket)
 
                 if shot.angle < 90:  # it's impossible to hit a ball at an angle of greater than 90 degrees
                     possible_shots.append(shot)  # add shot to array
@@ -71,18 +78,18 @@ class Table:
 
 
 class Ball:
-    def __init__(self, _type, x, y):
+    def __init__(self, _type, position):
         assert _type in ["cue", "eight", "solids", "stripes", "phantom_cue"]
 
         self.type = _type
-        self.position = x, y
+        self.position = position
         self.radius = 10
 
 
 class Pocket:
-    def __init__(self, x, y, radius=20):
-        self.position = x, y
-        self.radius = radius
+    def __init__(self, position):
+        self.position = position
+        self.radius = 20
 
 
 class Shot:
@@ -90,41 +97,56 @@ class Shot:
         self.cue_ball = cue_ball
         self.target_ball = target_ball
         self.pocket = pocket
-        self.angle = self.__calculate_angle()
 
-    # use cosine rule to calculate the angle the target ball must deviate to land in a pocket
-    def __calculate_angle(self):
-        a = Shot.__calculate_distance(self.target_ball.position, self.cue_ball.position)
-        b = Shot.__calculate_distance(self.target_ball.position, self.pocket.position)
-        c = Shot.__calculate_distance(self.cue_ball.position, self.pocket.position)
-        return 180 - self.__radians_to_degrees(math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b)))
-
-    # calculate distance between two points
-    @staticmethod
-    def __calculate_distance(position1, position2):
-        return math.sqrt((position1[0] - position2[0]) ** 2 + (position1[1] - position2[1]) ** 2)
-
-    @staticmethod
-    def __radians_to_degrees(radians):
-        return radians * 180 / math.pi
+        # use cosine rule to calculate the angle the target ball must deviate to land in a pocket
+        self.angle = 180 - Maths.angle_between_points(self.target_ball.position, self.cue_ball.position,
+                                                      self.pocket.position)
 
     # create "phantom" ball where cue ball makes contact with the target ball
     def create_phantom_cue_ball(self):
 
         # calculate gradient of line between pocket and target ball
-        gradient = - (self.pocket.position[1] - self.target_ball.position[1]) / \
-                   (self.pocket.position[0] - self.target_ball.position[0])
+        gradient = - (self.pocket.position.y - self.target_ball.position.y) / \
+                   (self.pocket.position.x - self.target_ball.position.x)
         angle = math.atan(gradient)  # angle of gradient
 
         # wizardry
-        if self.pocket.position[0] > self.target_ball.position[0]:
-            x = int(self.target_ball.position[0] - self.target_ball.radius * 2 * math.cos(angle))
-            y = int(self.target_ball.position[1] + self.target_ball.radius * 2 * math.sin(angle))
+        if self.pocket.position.x > self.target_ball.position.x:
+            x = int(self.target_ball.position.x - self.target_ball.radius * 2 * math.cos(angle))
+            y = int(self.target_ball.position.y + self.target_ball.radius * 2 * math.sin(angle))
         else:
-            x = int(self.target_ball.position[0] + self.target_ball.radius * 2 * math.cos(angle))
-            y = int(self.target_ball.position[1] - self.target_ball.radius * 2 * math.sin(angle))
+            x = int(self.target_ball.position.x + self.target_ball.radius * 2 * math.cos(angle))
+            y = int(self.target_ball.position.y - self.target_ball.radius * 2 * math.sin(angle))
 
-        return Ball("phantom_cue", x, y)
+        return Ball("phantom_cue", Point(x, y))
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def tuple(self):
+        return self.x, self.y
+
+
+class Maths:
+    # calculate distance between two points
+    @staticmethod
+    def calculate_distance(position1, position2):
+        return math.sqrt((position1.x - position2.x) ** 2 + (position1.y - position2.y) ** 2)
+
+    @staticmethod
+    def radians_to_degrees(radians):
+        return radians * 180 / math.pi
+
+    # given points a, b and c, returns angle at c using the cosine rule
+    @staticmethod
+    def angle_between_points(target, far1, far2):
+        a = Maths.calculate_distance(target, far1)
+        b = Maths.calculate_distance(target, far2)
+        c = Maths.calculate_distance(far1, far2)
+        return Maths.radians_to_degrees(math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b)))
 
 
 class Colour:
@@ -132,6 +154,7 @@ class Colour:
     SOLIDS = (255, 0, 0)
     STRIPES = (0, 255, 0)
     POCKET = (0, 0, 255)
+    RED = (0, 0, 255)
 
 
 if __name__ == "__main__":
@@ -141,22 +164,22 @@ if __name__ == "__main__":
     table = Table(image)
 
     # add pockets to table
-    table.pockets.append(Pocket(100, 100))
-    table.pockets.append(Pocket(400, 100))
-    table.pockets.append(Pocket(700, 100))
-    table.pockets.append(Pocket(100, 500))
-    table.pockets.append(Pocket(400, 500))
-    table.pockets.append(Pocket(700, 500))
+    table.pockets.append(Pocket(Point(100, 100)))
+    table.pockets.append(Pocket(Point(400, 100)))
+    table.pockets.append(Pocket(Point(700, 100)))
+    table.pockets.append(Pocket(Point(100, 500)))
+    table.pockets.append(Pocket(Point(400, 500)))
+    table.pockets.append(Pocket(Point(700, 500)))
 
     # add balls to table at random location
-    table.balls.append(Ball("cue", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("solids", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("solids", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("solids", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("solids", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("stripes", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("stripes", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("stripes", 150 + randint(0, 500), 150 + randint(0, 300)))
-    table.balls.append(Ball("stripes", 150 + randint(0, 500), 150 + randint(0, 300)))
+    table.balls.append(Ball("cue", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("solids", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("solids", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("solids", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("solids", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("stripes", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("stripes", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("stripes", Point(150 + randint(0, 500), 150 + randint(0, 300))))
+    table.balls.append(Ball("stripes", Point(150 + randint(0, 500), 150 + randint(0, 300))))
 
     table.show_best_shot("solids")
