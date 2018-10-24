136cd0c5e72a6400dc10703033007e96c3bedd23
HaydenMuscat24 <hayden.muscat24@gmail.com>
2018-10-24 22:51:51 +1100

    left kmeans.py in its sort of original state and moved to thresholding.py, my new sandpit

--- kMeans.py
+++ kMeans.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python3
 import sys
 import numpy as np
-from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture
+# from sklearn.cluster import KMeans
+# from sklearn.mixture import GaussianMixture
 import cv2
 import transform
 
-
 # take in the image then put all pixels in 1 array
 initial = cv2.imread(sys.argv[1])
-img = cv2.resize(initial, (600, 400))
+initial = cv2.resize(initial, (int(initial.shape[1]/4), int(initial.shape[0]/4)))
+img = initial.copy()
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # find the table hue and table mask
@@ -17,32 +18,8 @@ isFeltHue = transform.isFeltHueArray(feltHue)
 tableMask  = transform.getTableMask(hsv, feltHue, 'hsv')
 
 # --- Optional and expensive ---- hsv normalisations
-hsv = transform.normaliseSatAndVal(hsv, tableMask, 'hsv')
-hsv = transform.contrastSaturations(hsv, tableMask, 'hsv')
-img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-# get the balls
-circles = transform.getCircles(img, tableMask, 'bgr')
-for i in circles:
-    # draw the outer circle
-    cv2.circle(img,(i[0],i[1]),i[2]+1,(255,255,0),1)
-
-cv2.imshow("1", img)
-cv2.waitKey()
-sys.exit()
-
-colourPixels = []
-for i in range(len(hsv)):
-    for j in range(len(hsv[i])):
-        if isTable[i][j] and not isFeltHue[hsv[i][j][0]] and hsv[i][j][1] > 180:
-            colourPixels.append([img[i][j]])
-            included[i][j] = (255)
-        else:
-            included[i][j] = (0)
-
-colourPixels = np.array(colourPixels)
-colourPixels = colourPixels.reshape(-1, 3)
-imgPixels = img.reshape(-1, 3)
+# hsv = transform.normaliseSatAndVal(hsv, np.ones((hsv.shape[0], hsv.shape[1])), 'hsv')
+# img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
 #--------------------- gaussian mixes --------------------
 # gmm = GaussianMixture(n_components=6, covariance_type='full', max_iter=100, random_state=2)
